9187b6dfaf8f93fc004f0a2be7e9d20bd45361f6
nikita <nikmosyl@gmail.com>
2026-06-20 23:42:51 +0400

    clean system_stm32f1xx.c

--- CMSIS/Device/ST/STM32F1xx/Source/system_stm32f1xx.c
+++ CMSIS/Device/ST/STM32F1xx/Source/system_stm32f1xx.c
@@ -1,99 +1,97 @@
 #include "stm32f1xx.h"
 
 #if !defined(HSE_VALUE)
-#define HSE_VALUE 8000000U
+#define HSE_VALUE ((uint32_t)8000000)
 #endif
 
 #if !defined(HSI_VALUE)
-#define HSI_VALUE 8000000U
+#define HSI_VALUE ((uint32_t)8000000)
 #endif
+
+#define VECT_TAB_OFFSET 0x0
 
 uint32_t SystemCoreClock = HSI_VALUE;
 
-const uint8_t AHBPrescTable[16U] = {0,0,0,0,0,0,0,0,1,2,3,4,6,7,8,9};
-const uint8_t APBPrescTable[8U]  = {0,0,0,0,1,2,3,4};
-
-#if 0
-static void SetSysClockTo72(void)
+const uint8_t AHBPrescTable[16] =
 {
-    RCC->CR |= RCC_CR_HSEON;
-    while ((RCC->CR & RCC_CR_HSERDY) == 0U) {}
+    0,0,0,0,0,0,0,0,1,2,3,4,6,7,8,9
+};
 
-    FLASH->ACR = FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY_2;
-
-    RCC->CFGR = 0U;
-    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
-    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
-    RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
-
-    RCC->CFGR |= RCC_CFGR_PLLSRC;
-    RCC->CFGR |= RCC_CFGR_PLLXTPRE;
-    RCC->CFGR &= ~RCC_CFGR_PLLMULL;
-    RCC->CFGR |= RCC_CFGR_PLLMULL9;
-
-    RCC->CR |= RCC_CR_PLLON;
-    while ((RCC->CR & RCC_CR_PLLRDY) == 0U) {}
-
-    RCC->CFGR &= ~RCC_CFGR_SW;
-    RCC->CFGR |= RCC_CFGR_SW_PLL;
-    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL) {}
-}
-#endif
+const uint8_t APBPrescTable[8] =
+{
+    0,0,0,0,1,2,3,4
+};
 
 void SystemInit(void)
 {
-    RCC->CR |= RCC_CR_HSION;
-    RCC->CFGR = 0U;
-    RCC->CR &= ~(RCC_CR_PLLON | RCC_CR_CSSON | RCC_CR_HSEON);
-    RCC->CR &= ~RCC_CR_HSEBYP;
-    RCC->CIR = 0U;
+    RCC->CR |= 0x00000001;
 
-    /* Keep reset clock configuration: SYSCLK = HSI = 8 MHz. */
-    /* SetSysClockTo72(); */
+#if !defined(STM32F105xC) && !defined(STM32F107xC)
+    RCC->CFGR &= 0xF8FF0000;
+#else
+    RCC->CFGR &= 0xF0FF0000;
+#endif
 
-    SystemCoreClock = HSI_VALUE;
+    RCC->CR &= 0xFEF6FFFF;
+    RCC->CR &= 0xFFFBFFFF;
+    RCC->CFGR &= 0xFF80FFFF;
+
+#if defined(STM32F105xC) || defined(STM32F107xC)
+    RCC->CR &= 0xEBFFFFFF;
+    RCC->CIR = 0x00FF0000;
+    RCC->CFGR2 = 0x00000000;
+#elif defined(STM32F100xB) || defined(STM32F100xE)
+    RCC->CIR = 0x009F0000;
+    RCC->CFGR2 = 0x00000000;
+#else
+    RCC->CIR = 0x009F0000;
+#endif
+
+    SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET;
 }
 
 void SystemCoreClockUpdate(void)
 {
-    uint32_t tmp = RCC->CFGR & RCC_CFGR_SWS;
+    uint32_t tmp = 0;
+    uint32_t pllmull = 0;
+    uint32_t pllsource = 0;
 
-    if (tmp == RCC_CFGR_SWS_HSI)
-    {
-        SystemCoreClock = HSI_VALUE;
-    }
-    else if (tmp == RCC_CFGR_SWS_HSE)
-    {
-        SystemCoreClock = HSE_VALUE;
-    }
-    else if (tmp == RCC_CFGR_SWS_PLL)
-    {
-        uint32_t pllsrc = RCC->CFGR & RCC_CFGR_PLLSRC;
-        uint32_t pllmul = (RCC->CFGR & RCC_CFGR_PLLMULL) >> 18U;
-        pllmul += 2U;
+    tmp = RCC->CFGR & RCC_CFGR_SWS;
 
-        if (pllsrc == 0U)
-        {
-            SystemCoreClock = (HSI_VALUE >> 1U) * pllmul;
-        }
-        else
-        {
-            if ((RCC->CFGR & RCC_CFGR_PLLXTPRE) != 0U)
+    switch (tmp)
+    {
+        case 0x00:
+            SystemCoreClock = HSI_VALUE;
+            break;
+
+        case 0x04:
+            SystemCoreClock = HSE_VALUE;
+            break;
+
+        case 0x08:
+            pllmull = (RCC->CFGR & RCC_CFGR_PLLMULL) >> 18;
+            pllmull += 2;
+
+            pllsource = RCC->CFGR & RCC_CFGR_PLLSRC;
+
+            if (pllsource == 0)
             {
-                SystemCoreClock = (HSE_VALUE >> 1U) * pllmul;
+                SystemCoreClock = (HSI_VALUE >> 1) * pllmull;
             }
             else
             {
-                SystemCoreClock = HSE_VALUE * pllmul;
+                if (RCC->CFGR & RCC_CFGR_PLLXTPRE)
+                    SystemCoreClock = (HSE_VALUE >> 1) * pllmull;
+                else
+                    SystemCoreClock = HSE_VALUE * pllmull;
             }
-        }
-    }
-    else
-    {
-        SystemCoreClock = HSI_VALUE;
+            break;
+
+        default:
+            SystemCoreClock = HSI_VALUE;
+            break;
     }
 
-    uint32_t hpre = (RCC->CFGR & RCC_CFGR_HPRE) >> 4U;
-    uint32_t shift = AHBPrescTable[hpre];
-    SystemCoreClock >>= shift;
+    tmp = AHBPrescTable[(RCC->CFGR & RCC_CFGR_HPRE) >> 4];
+    SystemCoreClock >>= tmp;
 }
